c867f8ff60bd62feceaf74ece83a354bcf1166c8
NostrumBioDiscovery <daniel.soler@nostrumbiodiscovery.com>
2019-05-30 03:25:53 +0200

    Fix helpers test due to molecule class integration

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,6 +3,7 @@ from rdkit import Chem
 import os
 import glob
 from analogs_finder.helpers import helpers as hp
+from analogs_finder.search_methods import molecule as ml
 
 RESULT = 4
 DIR = os.path.dirname(__file__)
@@ -10,6 +11,7 @@ DB = os.path.join(DIR, "data/database.sdf")
 
 def test_duplicates(db=DB, result=RESULT):
     mols = Chem.SDMolSupplier(db)
+    mols = [ml.Molecule(m) for m in mols]
     output = hp.remove_duplicates(mols)
     assert len(output) == result
 
