32e727a37b64e74859f7a75bdd67fd3ec4ea5ba2
NostrumBioDiscovery <daniel.soler@nostrumbiodiscovery.com>
2019-05-30 02:52:23 +0200

    Fix remove duplicate to work with molecule class

--- analogs_finder/helpers/helpers.py
+++ analogs_finder/helpers/helpers.py
@@ -5,7 +5,7 @@ def remove_duplicates(molecules_duplicated):
     names = []
     mols_no_duplicates = []
     for mol in molecules_duplicated:
-        name = mol.GetProp("_Name")
+        name = mol.molecule.GetProp("_Name")
         if name in names:
             pass
         else:
