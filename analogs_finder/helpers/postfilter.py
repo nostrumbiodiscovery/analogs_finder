22c200ec212741f010ffe71d432921256e07c703
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-10-23 17:52:53 +0200

    Change align to getbestfit from rdkit to have into account tautomerization

--- analogs_finder/helpers/postfilter.py
+++ analogs_finder/helpers/postfilter.py
@@ -1,4 +1,5 @@
 from rdkit import Chem
+from rdkit.Chem import AllChem
 import numpy as np
 
 
@@ -13,17 +14,13 @@ def postfilter_mols(molecule_query, mol_most_similars, atoms_to_grow, atoms_to_a
     if avoid_repetition:
         #Broken
         mol_most_similars = remove_duplicates(mol_most_similars)
-    if any(atoms_to_grow):
-        coords_grow, neighbors_grow, indexes_grow = retrieve_atom_info(m_ref, atoms_to_grow)
-    if any(atoms_to_avoid):
-        coords_avoid, neighbors_avoid, indexes_avoid = retrieve_atom_info(m_ref, atoms_to_avoid)
     for m in reversed(mols_after_filter):
         if any(atoms_to_grow):
-            if not is_bonded(m_ref, m.molecule, atoms_to_grow, coords_grow, neighbors_grow, indexes_grow):
+            if not is_bonded(m_ref, m.molecule, atoms_to_grow):
                 mols_after_filter.remove(m)
                 continue
         if any(atoms_to_avoid):
-            if not is_not_bonded(m_ref, m.molecule, atoms_to_avoid, coords_avoid, neighbors_avoid, indexes_avoid):
+            if not is_not_bonded(m_ref, m.molecule, atoms_to_avoid):
                 mols_after_filter.remove(m)
                 continue
     return mols_after_filter
@@ -50,43 +47,44 @@ def retrieve_atom_info(m_ref, atoms):
     return coords, neighbors, indexes
 
 
-def is_bonded(m_ref, mol, atoms, coords, neighbors, indexes):
-    valid = False
+def is_bonded(m_ref, mol, atoms):
     try:
-        rmsd = Chem.rdMolAlign.AlignMol(mol, m_ref)
+        rmsd = Chem.rdMolAlign.GetBestRMS(mol, m_ref)
     except RuntimeError:
         try:
-            rmsd = Chem.rdMolAlign.AlignMol(m_ref, mol)
+            rmsd = Chem.rdMolAlign.GetBestRMS(m_ref, mol)
         except RuntimeError:
             return False
+    coords, neighbors, indexes = retrieve_atom_info(m_ref, atoms)
     for coord_ref, neighbour_ref, index_ref in zip(coords, neighbors, indexes):
         coords = mol.GetConformer(0).GetPositions()
         distances_to_atoms_from_ref = [np.linalg.norm(coord_ref - coord) for coord in coords]
         idx = np.argmin(distances_to_atoms_from_ref)
         min_distance_to_atomref = distances_to_atoms_from_ref[idx]
-        if distances_to_atoms_from_ref[idx] > 0.4:
+        if distances_to_atoms_from_ref[idx] == 0:
             return False
         atom = [ atom for i, atom in enumerate(mol.GetAtoms()) if i == idx][0]
         if len(atom.GetNeighbors()) > neighbour_ref:
+            w=Chem.SDWriter("trial{}.sdf".format(min_distance_to_atomref))
             return True
     return False
 
 
-def is_not_bonded(m_ref, mol, atoms, coords, neighbors, indexes):
-    valid = False
+def is_not_bonded(m_ref, mol, atoms):
     try:
-        rmsd = Chem.rdMolAlign.AlignMol(mol, m_ref)
+        rmsd = Chem.rdMolAlign.GetBestRMS(mol, m_ref)
     except RuntimeError:
         try:
-            rmsd = Chem.rdMolAlign.AlignMol(m_ref, mol)
+            rmsd = Chem.rdMolAlign.GetBestRMS(m_ref, mol)
         except RuntimeError:
             return False
+    coords, neighbors, indexes = retrieve_atom_info(m_ref, atoms)
     for coord_ref, neighbour_ref, index_ref in zip(coords, neighbors, indexes):
         coords = mol.GetConformer(0).GetPositions()
         distances_to_atoms_from_ref = [np.linalg.norm(coord_ref - coord) for coord in coords]
         idx = np.argmin(distances_to_atoms_from_ref)
         min_distance_to_atomref = distances_to_atoms_from_ref[idx]
-        if distances_to_atoms_from_ref[idx] > 0.4:
+        if distances_to_atoms_from_ref[idx] == 0:
             return False
         atom = [ atom for i, atom in enumerate(mol.GetAtoms()) if i == idx][0]
         if len(atom.GetNeighbors()) > neighbour_ref:
