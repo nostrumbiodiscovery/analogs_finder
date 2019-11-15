fda876ef5bca84380c67a785fdbea69da0fd7709
Nostrum <daniel.soler@e-campus.uab.cat>
2019-11-15 19:01:32 +0100

    Add --yes conda testing

--- analogs_finder/helpers/conda_test.sh
+++ analogs_finder/helpers/conda_test.sh
@@ -1,5 +1,5 @@
 conda uninstall analogs_finder
-conda install -c conda-forge -c rdkit -c nostrumbiodiscovery analogs_finder
+conda install -c conda-forge -c rdkit -c nostrumbiodiscovery analogs_finder --yes
 cd tests
 python -m pytest test_analysis.py test_fingerprints.py test_fusions.py test_methods.py test_helpers.py
 
