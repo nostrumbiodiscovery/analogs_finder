42116c789f60b05615f2f076ad09f4b009324028
Nostrum <daniel.soler@e-campus.uab.cat>
2019-11-15 18:57:35 +0100

    Test conda deployment

---
+++ analogs_finder/helpers/conda_test.sh
@@ -0,0 +1,5 @@
+conda uninstall analogs_finder
+conda install -c conda-forge -c rdkit -c nostrumbiodiscovery analogs_finder
+cd tests
+python -m pytest test_analysis.py test_fingerprints.py test_fusions.py test_methods.py test_helpers.py
+
